ba158f0fca910412187540649bd27fc691b57662
Ananya Jain <ananyajain386@gmail.com>
2024-01-19 19:41:36 +0530

    change

--- PAYWIND/settings.py
+++ PAYWIND/settings.py
@@ -158,7 +158,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ananyajain386@gmail.com'
-EMAIL_HOST_PASSWORD = 'yhvz cwpj fzxu jkbt'
+EMAIL_HOST_PASSWORD = '****'
 
 
 AUTH_USER_MODEL = 'app1.Paywind_User'
